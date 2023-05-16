f019645a525b3b37671c7226c28cbfda91ae4129
blu-exe <wowraphael@gmail.com>
2023-05-16 14:42:41 -0700

    added op2 array

--- mathgame/main.js
+++ mathgame/main.js
@@ -1,3 +1,4 @@
+// consts
 let viewportHeight = window.innerHeight;
 let viewportWidth = window.innerWidth;
 
@@ -8,22 +9,30 @@ const winNum = document.getElementsByClassName("winNum");
 const operand1 = document.querySelector("#operand1");
 const operand2 = document.querySelector("#operand2");
 
+// PID
+const numOp2 = 5;
+const maxNumOp2 = 12;
 let state = {
     playerATurn: true,
+    turnNum: 1,
+    opSelectA: "add",
+    opSelectB: "add",
+    
+    operation: "add",
     operand1: 1,
     operand2: 2,
     result: 2,
-    opSelectA: undefined,
-    opSelectB: undefined
+
+    op2List: []
 }
-
-
 let strToOpStr = {
     'add': '+',
-    'sub': '-',
+    'sub': '−',
     'mul': '×',
     'div': '÷'
 }
+
+
 
 // Setup event listeners
 $(".playerATable").click((e) => {
@@ -44,43 +53,11 @@ $(".playerBTable").click((e) => {
 });
 
 
-// Per-turn updates
-function setTurnIndicator() {
-    if (state.playerATurn) {
-        $(".bg").css("background", "linear-gradient(0deg, rgba(91,142,224,0.578890931372549) 0%, rgba(255,255,255,0) 15%)");
-    } else {
-        $(".bg").css("background", "linear-gradient(0deg, rgba(255,255,255,0) 0%, rgba(255,255,255,0) 85%, rgba(212,67,67,0.58) 100%)");
-    }
-}
-function op(r1, r2, op) {
-    if (op === "add") {
-        return r1 + r2;
-    } else if (op === "sub") {
-        return r1 - r2;
-    } else if (op === "mul") {
-        return r1 * r2;
-    } else if (op === "div") {
-        return r1 / r2;
-    }
-    throw new Error(`op() with invalid op ${op}`);
-}
-
-function update() {
-    if (state.playerATurn) {
-        $("#operation").text(strToOpStr[state.opSelectA]);
-        $("#result").text(op(state.operand1, state.operand2, state.opSelectA));
-
-    } else {
-        $("#operation").text(strToOpStr[state.opSelectB]);
-        $("#result").text(op(state.operand1, state.operand2, state.opSelectB));
-
-    }
-    setTurnIndicator();
-    state.playerATurn = !state.playerATurn;
-}
-
 
 // Game inits
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min) ) + min;
+}
 function guiGameStart() {
     for (var i = 0; i < winNum.length; ++i) {
         winNum[i].style.fontSize = "24pt";
@@ -110,17 +87,93 @@ function guiGameStart() {
         "width": (viewportWidth - 90) + "px"
     });
 
+    // transforms and ops, keep at end
+    for (let index = 0; index < state.op2List.length; index++) {
+        console.log("appending child " + state.op2List[index]);
+        let child = document.querySelector("#op2row").appendChild(document.createElement("div"));
+        child.classList.add("data");
+        child.classList.add("col");
+        child.setAttribute("id", "operand2");
+        child.innerHTML = state.op2List[index];
+    }
+
     $(".data").css({
         "fontSize": "40pt",
+        "text-align": "center",
         "transform": "rotate(-90deg)"
     });
 
     $("#operand1").text(state.operand1);
-    $("#operand2").text(state.operand2);
+    $("#result").text(state.result);
+    $("#operation").text(strToOpStr[state.operation]);
+}
+function funcGameStart() {
+    for (let index = 0; index < numOp2; index++) {
+        state.op2List[index] = getRandomInt(1, maxNumOp2);
+    }
+
+    state.operand1 = 1;
+    state.operand2 = state.op2List[2];
+    state.result = op(state.operand1, state.operand2, state.operation);
 }
 
 
+
+// Per-turn update helpers
+function op(r1, r2, op) {
+    console.log ("op(" + r1 + " " + op + " " + r2 + ")");
+    if (op === "add") {
+        return r1 + r2;
+    } else if (op === "sub") {
+        return r1 - r2;
+    } else if (op === "mul") {
+        return r1 * r2;
+    } else if (op === "div") {
+        return Math.floor(r1 / r2);
+    }
+    throw new Error(`op() with invalid op ${op}`);
+}
+function setTurnIndicator() {
+    if (state.playerATurn) {
+        if (state.turnNum > 2) {
+            $(".bg").removeClass("bg-reverse-animate");
+        }
+        $(".bg").addClass("bg-animate");
+    } else {
+        if (state.turnNum > 2) {
+            $(".bg").removeClass("bg-animate");
+        }
+        $(".bg").addClass("bg-reverse-animate");
+    }
+}
+function updateVars(s) {
+    if (s.playerATurn) {
+        s.operation = s.opSelectA;
+    } else {
+        s.operation = s.opSelectB;
+    }
+}
+function animateUpdate (olds, s) {
+    $("#operation").text(strToOpStr[s.operation]);
+    $("#result").text(op(s.operand1, s.operand2, s.operation));
+}
+
+
+
+// Per-turn update
+function update() {
+    let oldstate = state;
+    updateVars(state);
+    animateUpdate(oldstate, state);
+    setTurnIndicator();
+    state.playerATurn = !state.playerATurn;
+    state.turnNum++;
+}
+
+
+
+// Main
 $(document).ready(() => {
+    funcGameStart();
     guiGameStart();
-    // initAnimation();
 });
